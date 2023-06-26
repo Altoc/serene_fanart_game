d84cdffb492e14b113e17d13f7a237d889eb98b2
Ian Whitesel <altocgamedev@gmail.com>
2023-06-26 16:56:45 -0400

    better timer

--- scripts/timerLabel.gd
+++ scripts/timerLabel.gd
@@ -2,34 +2,34 @@ extends Label
 
 @onready var SIGNAL_BUS = get_node("/root/Main/SignalBus")
 
-@onready var mins = 0
-@onready var secs = 0
+@onready var minsRaw = 0
+@onready var secsRaw = 0
 
-@onready var trimmedStr = ""
+@onready var timeStr = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SIGNAL_BUS.SET_UI_MODE.connect(onUiModeChange)
 
 func onUiModeChange(argMode):
-	mins = 0
-	secs = 0
+	minsRaw = 0
+	secsRaw = 0
 	if(argMode == 2):
-		SIGNAL_BUS.emit_signal("FINAL_TIME", trimmedStr)
+		SIGNAL_BUS.emit_signal("FINAL_TIME", timeStr)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#timer += delta
-	secs += delta
+	secsRaw += delta
+	var secs = roundi(secsRaw)
 	if(secs >= 60):
-		secs = 0
-		mins += 1
-	var minStr = str(mins)
-	if(mins == 0):
+		secsRaw = 0
+		minsRaw += 1
+	var minStr = str(minsRaw)
+	if(minsRaw == 0):
 		minStr = "00"
 	var secStr = str(secs)
-	if(secs - 10 < 0):
-		secStr = str(roundi(secs))
-	var timeStr = minStr + ":" + secStr
-	trimmedStr = timeStr.substr(0, timeStr.find(":", 0)) + timeStr.substr(timeStr.find(":", 0), 3)
-	text = trimmedStr
+	if(secs < 10):
+		secStr = "0" + secStr
+	timeStr = minStr + ":" + secStr
+	#trimmedStr = timeStr.substr(0, timeStr.find(":", 0)) + timeStr.substr(timeStr.find(":", 0), 3)
+	text = timeStr
